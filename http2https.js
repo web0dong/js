a7fd1d1cc2dc6ee124ee020171075c2dcc1c1418
hoangduit <hoangduit@gmail.com>
2017-07-12 13:15:07 +0700

    Update http2https.js

--- http2https.js
+++ http2https.js
@@ -1,4 +1,7 @@
 function replaceHttp2Https(e)
 {
-document.write (e.replace("http://", "https://"));
+var mmmmmm = '<data:blog.url/>';
+
+var nn = mmmmmm.replace("https","http")
+document.write("<title>"+nn+"</title>");
 }
